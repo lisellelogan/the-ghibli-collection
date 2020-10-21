8e7e309ec05cef60441bdcd40ad6210d5da9beb6
lisellelogan <liselle.logan@yahoo.com>
2020-10-20 21:03:41 -0400

    utilized data from api to create attributes in film class

--- lib/api.rb
+++ lib/api.rb
@@ -2,11 +2,19 @@ class API
 
     def self.get_films
         url = "https://ghibliapi.herokuapp.com/films"
-        binding.pry
         uri = URI(url)
         response = Net::HTTP.get(uri)
-        hash = JSON.parse(response)
-
+        array_of_films = JSON.parse(response)
+        
+        array_of_films.each do |film_hash|
+            film = Film.new
+            film.title = film_hash["title"]
+            film.description = film_hash["description"]
+            film.director = film_hash["director"]
+            film.producer = film_hash["producer"]
+            film.release_date = film_hash["release_date"]
+            film.rt_score = film_hash["rt_score"]
+        end
     end
 
 end
--- lib/film.rb
+++ lib/film.rb
@@ -1,3 +1,14 @@
 class Film
+    
+    attr_accessor :title, :description, :director, :producer, :release_date, :rt_score
 
+    @@all = []
+
+    def initialize
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
